70183b2bc629e5ad8a8fc61ffad2c277ec4ef5c9
Arno Moonens <x-006@hotmail.com>
2015-12-06 12:35:07 +0100

    Fixed grid ticks labels and positions

--- print_grid.py
+++ print_grid.py
@@ -11,6 +11,15 @@ n = len(grid[0])
 ax = plt.gca()
 ax.xaxis.set_ticks(range(1,n+1))
 ax.yaxis.set_ticks(range(1,m+1))
+ax.get_xaxis().set_ticklabels([])
+ax.axes.get_yaxis().set_ticklabels([])
+
+ax2 = ax.twinx()
+#ax2.xaxis.set_ticks([float(x)/2 for x in range(2*n)])
+ax2.yaxis.set_ticks([float(x)/2 for x in range(2*m+1)])
+ax3 = ax.twiny()
+ax3.xaxis.set_ticks([float(x)/2 for x in range(2*n+1)])
+
 # create a grid
 ax.grid(True, linestyle='-', linewidth=1)
 # put the grid below other plot elements
@@ -36,6 +45,11 @@ for i in range(m):
                     transform=ax.transAxes)
 ax.axhline(y=m,linewidth=4,color='k')
 ax.axvline(x=n,linewidth=4,color='k')
+ax2.set_yticklabels([i/2 if i % 2 else '' for i in range(2*m)], minor=False)
+ax3.set_xticklabels([i/2 if i % 2 else '' for i in range(2*n)],minor=False)
+ax2.tick_params(axis=u'both', which=u'both',length=0)
+ax3.tick_params(axis=u'both', which=u'both',length=0)
+ax2.yaxis.set_ticks_position('left')
+ax3.xaxis.set_ticks_position('bottom')
 plt.draw()
-
 plt.show()
