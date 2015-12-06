1a0faa68ff2cc7753980c1d6a89149147ceb3bb5
Arno Moonens <x-006@hotmail.com>
2015-12-06 10:18:06 +0100

    Made separate function to check for walls

--- print_grid.py
+++ print_grid.py
@@ -3,7 +3,7 @@ from ast import literal_eval
 grid = []
 with open("grid.txt") as f:
     for line in f:
-        grid.insert(0,literal_eval(line[:-1]))
+        grid.insert(0,literal_eval(line[:-1])) #Opposite order
 
 m = len(grid)
 n = len(grid[0])
@@ -16,13 +16,19 @@ ax.grid(True, linestyle='-', linewidth=1)
 # put the grid below other plot elements
 ax.set_axisbelow(True)
 
+def vertical_wall(walls):
+    return walls & 1
+
+def horizontal_wall(walls):
+    return walls & 2
+
 for i in range(m):
     for j in range(n):
         walls = grid[i][j][0]
         flag = grid[i][j][1]
-        if walls & 1:
+        if vertical_wall(walls):
             ax.axvline(x=j,ymin=float(i)/m,ymax=float(i+1)/m,linewidth=4, color='k')
-        if walls & 2:
+        if horizontal_wall(walls):
             ax.axhline(y=i,xmin=float(j)/n,xmax=float(j+1)/n,linewidth=4, color='k')
         if flag:
             ax.text(float(j+0.5)/n, float(i+0.5)/m, flag, fontsize=12,horizontalalignment='center',
